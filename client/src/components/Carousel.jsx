fae706bda36abb62b539061440d3dc9a71830393
IsaacMota08 <isaacmota08@hotmail.com>
2025-06-08 21:06:04 -0400

    modified:   client/src/components/Carousel.jsx 	deleted:    client/public/assets/img/e1.jpeg 	deleted:    client/public/assets/img/e2.jpeg 	deleted:    client/public/assets/img/e3.jpeg 	modified:   client/src/components/Carousel.jsx 	modified:   client/src/pages/Cliente/Gallery.jsx 	modified:   client/src/styles/Gallery.css 	client/public/assets/img/Fauna/ 	client/public/assets/img/Flora/

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import '../styles/Carrusel.css'; // Asegúrate de que este CSS exista
+import React, { useState } from "react";
+import "../styles/Carrusel.css"; // Asegúrate de que este CSS exista
 
 const images = [
-  '/assets/img/e1.jpeg',
-  '/assets/img/e2.jpeg',
-  '/assets/img/e3.jpeg',
+  "/assets/img/e1.jpeg",
+  "/assets/img/e2.jpeg",
+  "/assets/img/e3.jpeg",
   // Agrega más rutas según tus imágenes
 ];
 
@@ -23,21 +23,27 @@ const Carrusel = () => {
     );
   };
 
+  const imagenActual = images[currentIndex];
+
   return (
     <div className="carousel-container">
-      <button className="nav-btn left" onClick={handlePrev}>
-        ◀
-      </button>
+      <button className="nav-btn left" onClick={handlePrev}></button>
 
       <img
-        src={images[currentIndex]}
-        alt={`Slide ${currentIndex + 1}`}
+        src={imagenActual.src}
+        alt={imagenActual.nombre}
         className="carousel-image"
       />
 
-      <button className="nav-btn right" onClick={handleNext}>
-        ▶
-      </button>
+      <button className="nav-btn right" onClick={handleNext}></button>
+
+      {/* Características visibles solo cuando hay una imagen seleccionada */}
+      <div className="caracteristica-box">
+        <h2>{imagenActual.nombre}</h2>
+        <p>
+          <strong>Características:</strong> {imagenActual.caracteristica}
+        </p>
+      </div>
     </div>
   );
 };
