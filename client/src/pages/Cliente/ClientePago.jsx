624c9c2fb1d48aafaa8482889fd47be3bbd5bfe3
Natnael Del Rosario Daly <96176909+BurquerRayon@users.noreply.github.com>
2025-06-23 07:52:42 -0400

    modified:   client/src/pages/Cliente/ClientePago.jsx 	new file:   client/src/styles/PagoCliente.css 	modified:   client/src/styles/ReservaCliente.css

--- client/src/pages/Cliente/ClientePago.jsx
+++ client/src/pages/Cliente/ClientePago.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
+import '../../styles/PagoCliente.css';
 
 const ClientePago = () => {
   const { id_reserva } = useParams();
