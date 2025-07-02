094e8da99c08d79813e9b26427ae82c6417ea4ab
IsaacMota08 <Isaacmota08@hotmail.com>
2025-07-02 09:01:08 -0400

    Integré mis cambios locales con los remotos después del pull

---
+++ client/src/pages/Cliente/Cuidado.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import "../../styles/Cuidado.css";
+import Carrusel from "../../components/Carousel";
+import Footer from "../../components/Footer";
+
+//Desarrollar una sección informativa que oriente sobre las mejores prácticas de conservación y protección del ecosistema. //
+function App() {
+  return (
+    <div>
+      <div className="cuidado-container">
+        <h1 className="cuidado-title">Conservacion de la especie</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
